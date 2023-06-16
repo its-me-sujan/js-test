fa9f0482ae4560d0c01d8d63a8812b79bca4315d
its-me-sujan <itsmesujangit@gmail.com>
2023-06-16 13:58:27 +0545

    setting up js

---
+++ index.js
@@ -0,0 +1,21 @@
+//using var keyword
+var a = "123";
+console.log(a);
+
+//using let keyword
+let b = '456';
+console.log(b);
+
+//using const keyword
+const c = 23;
+console.log(c);
+
+// alert('hello world');
+
+// const age = prompt('how old are you?');
+// console.log(age);
+
+// const isStd = confirm('Are you an Student?');
+// console.log(isStd);
+
+
