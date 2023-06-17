2856afee467a8e291dd2bf8975581e819d9a33b6
its-me-sujan <itsmesujangit@gmail.com>
2023-06-17 15:57:06 +0545

    completed task0

--- index.js
+++ index.js
@@ -1,67 +1,72 @@
-//using var keyword
-var a = "123";
-console.log(a);
+// //using var keyword
+// var a = "123";
+// console.log(a);
 
-//using let keyword
-let b = '456';
-console.log(b);  
+// //using let keyword
+// let b = '456';
+// console.log(b);  
 
-//using const keyword
-const c = 23; 
-console.log(c);  
-// alert('hello world');
+// //using const keyword
+// const c = 23; 
+// console.log(c);  
+// // alert('hello world');
  
-// const age = prompt('how old are you?');
-// console.log(age);
+// // const age = prompt('how old are you?');
+// // console.log(age);
    
-// const isStd = confirm('Are you an Student?');
-// console.log(isStd);
+// // const isStd = confirm('Are you an Student?');
+// // console.log(isStd);
 
 
-let x = 1;
-let y = 2;
-//using if statement
-if (x == 1){
-    console.log('x is 1');
-} 
-//using && operator
-if (x === 1 && y === 2){
-    console.log("&& operator tested");
-}
-//using || operator
-if((x = 1) || (y = 3)){
-    console.log("|| operator tested");
-}
-if(x != 3){
-    console.log("! operator tested");
-}
-//using ternary operator
-if (x === 1){
-    console.log("x is 1");
-}else{
-    console.log("x is not 1");
-}
-
-let result = x === 1 ? "x is 1" : "x is not 1";
-// result = x === 1? "x is 1" : x === 2 ? "x is 2" : "x is not 1 or 2";
-console.log(result);
-
-//for loops
-
-// for(let i = 0; i < 10; i++){
-//     console.log(i);
+// let x = 1;
+// let y = 2;
+// //using if statement
+// if (x == 1){
+//     console.log('x is 1');
+// } 
+// //using && operator
+// if (x === 1 && y === 2){
+//     console.log("&& operator tested");
+// }
+// //using || operator
+// if((x = 1) || (y = 3)){
+//     console.log("|| operator tested");
+// }
+// if(x != 3){
+//     console.log("! operator tested");
+// }
+// //using ternary operator
+// if (x === 1){
+//     console.log("x is 1");
+// }else{
+//     console.log("x is not 1");
 // }
 
-//function
+// let result = x === 1 ? "x is 1" : "x is not 1";
+// // result = x === 1? "x is 1" : x === 2 ? "x is 2" : "x is not 1 or 2";
+// console.log(result);
 
-// function add(a,b){
-//     console.log(a + b);
-// }
+// //for loops
 
-// add(2,3);
+// // for(let i = 0; i < 10; i++){
+// //     console.log(i);
+// // }
 
-const add = (a,b) => {
-    console.log(a+b);
-};
+// //function
 
-add(3,4);  
+// // function add(a,b){
+// //     console.log(a + b);
+// // }
+
+// // add(2,3);
+
+// const add = (a,b) => {
+//     console.log(a+b);
+// };
+
+// add(3,4);  
+
+//task 0
+console.log("Hello world");
+const PI = 3.14;
+console.log(PI);
