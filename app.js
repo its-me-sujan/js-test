2680d0b2a4f73b9fab26784c39e9550e885b4832
its-me-sujan <itsmesujangit@gmail.com>
2023-06-21 14:49:09 +0545

    file operation is done

--- app.js
+++ app.js
@@ -11,6 +11,10 @@
 
 const fs = require('fs');
 const rs = fs.createReadStream('./demofile.txt');
-rs.on('open', () =>{
-    console.log('The file is open');;
-})
+rs.on('open', () => { 
+    console.log('The file is open');
+});
+
+rs.on('data', (data) => {
+    console.log(data.toString());
+});
