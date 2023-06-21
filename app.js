4c444c1b0a9e233b0cff674670a7156708edbba7
its-me-sujan <itsmesujangit@gmail.com>
2023-06-21 13:56:59 +0545

    created module is used

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
+const currDateTime = require('./myFirstModeule');
 
 http.createServer((req,res) => {
     res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello Wqewrewasdfrorld!');
-}).listen(8080);
-console.log('app running on port 8080');
+    res.end('Hello World!' + currDateTime.myDateTime());
+}).listen(8090);
+
