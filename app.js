df938c0c94afeee9bdb9f1b61e26ba7002758a17
its-me-sujan <itsmesujangit@gmail.com>
2023-06-21 15:01:10 +0545

    learning events of nodejs

--- app.js
+++ app.js
@@ -9,12 +9,25 @@
 // }).listen(poort);
 // console.log(`app running ${poort}`)
 
-const fs = require('fs');
-const rs = fs.createReadStream('./demofile.txt');
-rs.on('open', () => { 
-    console.log('The file is open');
-});
+// const fs = require('fs');
+// const rs = fs.createReadStream('./demofile.txt');
+// rs.on('open', () => { 
+//     console.log('The file is open');
+// });
 
-rs.on('data', (data) => {
-    console.log(data.toString());
-});
+// rs.on('data', (data) => {
+//     console.log(data.toString());
+// });
+
+//initializing eventemitter
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
+
+//what event handler does
+const myEventHandler = () => {
+    console.log('I hear a scream');
+};
+//assign the event handler to an event
+eventEmitter.on('scream', myEventHandler);
+//fire the scream event
+eventEmitter.emit('scream');
