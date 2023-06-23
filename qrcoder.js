b529ef1dc066366d1d91e1d28cb849945eb32227
its-me-sujan <itsmesujangit@gmail.com>
2023-06-23 21:55:06 +0545

    Installed readline module

--- qrcoder.js
+++ qrcoder.js
@@ -1,4 +1,12 @@
 const QRCode = require('qrcode')
-QRCode.toString('sujan shrestha', {type:'terminal'}, function (err, url){
-    console.log(url)
-})   
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Enter a string: ', (userInput) => {
+    QRCode.toString(userInput, {type:'terminal'}, function (err, url){
+        console.log(url)
+    })
+    rl.close();
+});
